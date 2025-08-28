155cfebfe4a3b33c46d2de1059af53b89cb24de6
Tonyo0810 <larviolordfrance1@gmail.com>
2025-08-28 10:53:44 +0800

    color for buttons

--- app/goals/create.jsx
+++ app/goals/create.jsx
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
   },
   button: {
     padding: 18,
-    backgroundColor: '#22a8d1ff',
+    backgroundColor: '#83c914ff',
     color: 'white',
     borderRadius: 8,
   }
--- app/index.jsx
+++ app/index.jsx
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
   link: {
     marginVertical: 20,
     padding: 16,
-    backgroundColor: '#22a8d1ff',
+    backgroundColor: '#83c914ff',
     color: 'white',
     borderRadius: 8,
   },
